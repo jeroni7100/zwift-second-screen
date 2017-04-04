6b569070e5309246071548f8a5d991efd1b5935d
Jesper Rosenlund Nielsen <jesper@rosenlundnielsen.dk>
2017-04-04 16:43:04 +0200

    changes in rider.js

--- server/rider.js
+++ server/rider.js
@@ -138,7 +138,7 @@ class Rider extends EventEmitter {
             playerType: rider.playerType,
             contryAlpha3: rider.countryAlpha3,
             countryCode: rider.countryCode,
-            TEMP: 'TEMP', weight: rider.weight }, status);
+            weight: rider.weight }, status);
           //   distance: status.distance,
           //   speed: status.speed,
           //   power: status.power,
@@ -173,9 +173,8 @@ class Rider extends EventEmitter {
         });
   }
 
-  addStatic(status) {
+  addExtra(status) {
       const extra = {
-          // wattsPerKG: this.getWattsPerKg(status.power, this.static.weight),
           roadID: status.roadID,
           rideOns: status.rideOns,
           isTurning: status.isTurning,
@@ -195,7 +194,7 @@ class Rider extends EventEmitter {
       this.account.getWorld(1).riderStatus(id)
           .then(status => {
             // resolve(status);
-            resolve(this.addStatic(status));
+            resolve(this.addExtra(status));
           })
           .catch(ex => {
             console.log(`Failed to get status for ${id}${errorMessage(ex)}`);
